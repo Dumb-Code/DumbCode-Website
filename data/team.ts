5ad2404fff31f9691cdce2339570d108b970cc1f
Brandon Davis <neusfear@gmail.com>
2025-09-14 15:35:46 -0500

    Update team info

--- data/team.ts
+++ data/team.ts
@@ -10,7 +10,7 @@ export const primaryMembers: PrimaryMemberType[] = [
         "bio": "Brownboii is the team's lead Modeler, their main contributions to the team are the machine & fence models and most of Project Nublar's dinosaur models.",
         "roles": ["modeler"],
         "socials": [
-            { "platform": "discord", "link": "Brownboiiwonder#0357" },
+            { "platform": "discord", "link": "brownboiiwonder" },
             { "platform": "github", "link": "https://github.com/kashgummaraju%22%7D" }
         ]
     },
@@ -20,7 +20,7 @@ export const primaryMembers: PrimaryMemberType[] = [
         "bio": "Wyn Price is the DumbCode programming mastermind. His efforts include programming the Mods, and the backend of the new DumbCode Studio.",
         "roles": ["programmer", "web_developer"],
         "socials": [
-            { "platform": "discord", "link": "WynPrice#0001" },
+            { "platform": "discord", "link": "wynprice" },
             { "platform": "twitter", "link": "https://twitter.com/wyn_price" },
             { "platform": "github", "link": "https://github.com/Wyn-Price" },
             { "platform": "youtube", "link": "https://www.youtube.com/channel/UCUh1aG2Aa1m59kwzVlBjojA" }
@@ -32,7 +32,7 @@ export const primaryMembers: PrimaryMemberType[] = [
         "bio": "NeusFear is the team's Web frontend Developer, their main contributions to the team are the design of the DumbCode Studio, the websites, among various asset contributions for the Project Nublar Mod",
         "roles": ["programmer", "web_developer"],
         "socials": [
-            { "platform": "discord", "link": "NeusFear#5173" },
+            { "platform": "discord", "link": "neusfear" },
             { "platform": "twitter", "link": "https://twitter.com/NeusFear" },
             { "platform": "github", "link": "https://github.com/NeusFear" },
             { "platform": "youtube", "link": "https://www.youtube.com/channel/UCVNgt2sHMnhpIveeKeSAyVQ" },
@@ -46,7 +46,7 @@ export const primaryMembers: PrimaryMemberType[] = [
         "bio": "Brotat is one of the team's main texture artists for the Project Nublar Mod. He works on both the dinosaurs and non dinosaur textures in order to make sure everything flows smoothly and the mod is released at a top notch quality.",
         "roles": ["texture_artist"],
         "socials": [
-            { "platform": "discord", "link": "Brotat#7369" },
+            { "platform": "discord", "link": "brotat" },
             { "platform": "twitter", "link": "https://twitter.com/Sunking64" },
             { "platform": "github", "link": "https://github.com/Brotatsun64" },
             { "platform": "youtube", "link": "https://www.youtube.com/channel/UCGfffU8rEX43SmACBAFjUsQ" }
@@ -58,7 +58,7 @@ export const primaryMembers: PrimaryMemberType[] = [
         "bio": "Mazikeen is one of the team's main texture artists, along with various asset contributions for the mod, Mazikeen is also part of the build team that is responsible for the asset showcases along with any in game builds related to the Project Nublar Mod",
         "roles": ["texture_artist"],
         "socials": [
-            { "platform": "discord", "link": "Mazikeen#7846" },
+            { "platform": "discord", "link": "maziikeenz" },
             { "platform": "twitter", "link": "https://twitter.com/Mazikeen143" },
         ]
     },
@@ -68,7 +68,7 @@ export const primaryMembers: PrimaryMemberType[] = [
         "bio": "Hyper is one of the animators for DumbCode. He contributes to the lifelike movement of most of the animals from Project Nublar.",
         "roles": ["animator"],
         "socials": [
-            { "platform": "discord", "link": "Hyper#5603" },
+            { "platform": "discord", "link": "notthathyper" },
             { "platform": "twitter", "link": "https://twitter.com/NotThatHyper" },
         ]
     }
@@ -80,19 +80,9 @@ export const currentMembers: CurrentMemberType[] = [
         "imageName": "sindavar.png",
         "roles": ["texture_artist", "modeler"],
         "socials": [
-            { "platform": "discord", "link": "Sindavar#0303" },
+            { "platform": "discord", "link": "sindavar" },
             { "platform": "twitter", "link": "https://twitter.com/sindavar33" },
             { "platform": "github", "link": "https://github.com/Sindavar" },
-        ]
-    },
-    {
-        "name": "Gegy",
-        "imageName": "gegy.png",
-        "roles": ["programmer"],
-        "socials": [
-            { "platform": "discord", "link": "Gegy#9970" },
-            { "platform": "twitter", "link": "https://twitter.com/gegy1000" },
-            { "platform": "github", "link": "https://github.com/Gegy" }
         ]
     },
     {
@@ -100,77 +90,19 @@ export const currentMembers: CurrentMemberType[] = [
         "imageName": "lucca.png",
         "roles": ["graphic_designer", "concept_artist"],
         "socials": [
+            { "platform": "discord", "link": "lucca2951" },
             { "platform": "twitter", "link": "https://twitter.com/Lucca2951" },
             { "platform": "artstation", "link": "https://www.artstation.com/lucca2951" },
         ]
     },
     {
-        "name": "FloofHips",
-        "imageName": "floofhips.jpg",
-        "roles": ["texture_artist"],
-        "socials": [
-            { "platform": "discord", "link": "Reda#8727" },
-            { "platform": "twitter", "link": "https://twitter.com/FloofHips" },
-            { "platform": "github", "link": "https://github.com/FloofHips" },
-            { "platform": "youtube", "link": "https://youtube.com/c/FloofHips%22%7D" }
-        ]
-    },
-    {
-        "name": "Katostrophic",
-        "imageName": "katostrophic.jpg",
-        "roles": ["modeler", "texture_artist"],
-        "socials": [
-            { "platform": "discord", "link": "katostrophic#7421" },
-            { "platform": "twitter", "link": "https://twitter.com/kat0strophic" },
-            { "platform": "github", "link": "https://github.com/kat0strophic" }
-        ]
-    },
-    {
-        "name": "thebradqq",
-        "imageName": "bbq.png",
-        "roles": ["modeler", "texture_artist"],
-        "socials": [
-            { "platform": "discord", "link": "thebbq#8506" },
-            { "platform": "github", "link": "https://github.com/bradleyq" },
-            { "platform": "youtube", "link": "https://www.youtube.com/c/thebradqq" }
-        ]
-    },
-    {
-        "name": "MX",
-        "imageName": "mx.jpg",
-        "roles": ["animator"],
-        "socials": [
-            { "platform": "discord", "link": "emmmmm#7181" },
-        ]
-    },
-    {
-        "name": "Rodentman87",
-        "imageName": "rodentman87.png",
-        "roles": ["programmer"],
-        "socials": [
-            { "platform": "discord", "link": "Rodentman87#8787" },
-            { "platform": "twitter", "link": "https://twitter.com/rodentman87" },
-            { "platform": "github", "link": "https://github.com/rodentman87" },
-        ]
-    },
-    {
-        "name": "Elyasisly",
+        "name": "Omar",
         "imageName": "elyasisly.jpg",
-        "roles": ["sound_artist"],
+        "roles": ["sound_artist", "animator"],
         "socials": [
-            { "platform": "discord", "link": "Elyasisly#5933" },
+            { "platform": "discord", "link": "omar_tuareg" },
             { "platform": "twitter", "link": "https://twitter.com/Ely27290042" },
             { "platform": "github", "link": "https://github.com/Elysisy" }
-        ]
-    },
-    {
-        "name": "Mr.BizarreMegaloceros",
-        "imageName": "bizarrealtispinax.webp",
-        "roles": ["modeler"],
-        "socials": [
-            { "platform": "discord", "link": "Mr.BizarreMegaloceros#1993" },
-            { "platform": "twitter", "link": "https://twitter.com/BAltispinax" },
-            { "platform": "github", "link": "https://github.com/BizarreAltispinax" }
         ]
     },
     {
@@ -178,9 +110,17 @@ export const currentMembers: CurrentMemberType[] = [
         "imageName": "fred.png",
         "roles": ["texture_artist"],
         "socials": [
-            { "platform": "discord", "link": "Fred.#5269" },
+            { "platform": "discord", "link": "fredleon" },
             { "platform": "twitter", "link": "https://twitter.com/_ItzFred" },
             { "platform": "github", "link": "https://github.com/ItzFred" }
+        ]
+    },
+    {
+        "name": "ShadowTek",
+        "imageName": "shadowtek.jpg",
+        "roles": ["programmer"],
+        "socials": [
+            { "platform": "discord", "link": "qwort_int" }
         ]
     }
 ]
@@ -245,6 +185,34 @@ export const pastMembers: PastMembersType[] = [
     {
         "name": "Cucho",
         "imageName": "chucho.png"
+    },
+    {
+        "name": "Mr.BizarreMegaloceros",
+        "imageName": "bizarrealtispinax.webp"
+    },
+    {
+        "name": "Gegy",
+        "imageName": "gegy.png"
+    },
+    {
+        "name": "Rodentman87",
+        "imageName": "rodentman87.png"
+    },
+    {
+        "name": "MX",
+        "imageName": "mx.jpg"
+    },
+    {
+        "name": "FloofHips",
+        "imageName": "floofhips.jpg"
+    },
+    {
+        "name": "Katostrophic",
+        "imageName": "katostrophic.jpg"
+    },
+    {
+        "name": "thebradqq",
+        "imageName": "bbq.png"
     }
 ]
 
